b02ee742921c89ea16dd0cddca1c5b571024c65f
Navdeep Singh <31474502+bhangunavi@users.noreply.github.com>
2024-12-17 13:41:35 +0530

    Add files via upload

---
+++ excercise.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class excercise {
+    // static int a = 46;
+    // static int b = 23;
+    // static int c = 675;
+    // static int avg = (a + b + c) / 3;
+    // static int side = 5;
+    // static int area = side * side;
+
+    public static void main(String[] args) {
+        // int age = 
+           //if (age > 18) {
+
+        //     System.out.println("you are adult");
+        // } else {
+        //     System.out.println("you are not adult");
+        // }
+        // System.out.println("AREA OF SQUARE IS:" + area); System.out.println("THE
+        // AVERAGE OF NUMS IS:" + avg);
+    
+            Scanner sc=new Scanner(System.in);
+            int num=sc.nextInt();
+            if(num%2==0){
+                System.out.println("num is even");
+                }else{
+                    System.out.println("num is odd");
+            }
+    }
+}
